6553e609d5845210c4022b2054412930aca9b0d0
S_A_T <avtobot.08@gmail.com>
2014-12-17 21:02:02 +0200

    Start
    Создана форма

---
+++ ComputerAutoShotdown/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace ComputerAutoShotdown
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.settingsButton = new System.Windows.Forms.Button();
+            this.hours = new System.Windows.Forms.TextBox();
+            this.minutes = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // settingsButton
+            // 
+            this.settingsButton.BackgroundImage = global::ComputerAutoShotdown.Properties.Resources.settings;
+            this.settingsButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.settingsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.settingsButton.Location = new System.Drawing.Point(449, 12);
+            this.settingsButton.Name = "settingsButton";
+            this.settingsButton.Size = new System.Drawing.Size(23, 23);
+            this.settingsButton.TabIndex = 0;
+            this.settingsButton.UseVisualStyleBackColor = true;
+            // 
+            // hours
+            // 
+            this.hours.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.hours.BackColor = System.Drawing.Color.Black;
+            this.hours.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.hours.Font = new System.Drawing.Font("Microsoft Sans Serif", 55F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)), true);
+            this.hours.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.hours.Location = new System.Drawing.Point(122, 170);
+            this.hours.MaxLength = 2;
+            this.hours.Name = "hours";
+            this.hours.Size = new System.Drawing.Size(100, 84);
+            this.hours.TabIndex = 1;
+            this.hours.Text = "17";
+            this.hours.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // minutes
+            // 
+            this.minutes.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.minutes.BackColor = System.Drawing.Color.Black;
+            this.minutes.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.minutes.Font = new System.Drawing.Font("Microsoft Sans Serif", 55F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)), true);
+            this.minutes.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.minutes.Location = new System.Drawing.Point(264, 170);
+            this.minutes.MaxLength = 2;
+            this.minutes.Name = "minutes";
+            this.minutes.Size = new System.Drawing.Size(100, 84);
+            this.minutes.TabIndex = 2;
+            this.minutes.Text = "25";
+            this.minutes.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox4.BackColor = System.Drawing.Color.Black;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 55F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)), true);
+            this.textBox4.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.textBox4.Location = new System.Drawing.Point(228, 170);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(30, 84);
+            this.textBox4.TabIndex = 4;
+            this.textBox4.Text = ":";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(460, 31);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Комп\'ютер вимкнеться в заданий час!";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = global::ComputerAutoShotdown.Properties.Resources.shutdown;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(484, 266);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.minutes);
+            this.Controls.Add(this.hours);
+            this.Controls.Add(this.settingsButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(1000, 700);
+            this.MinimumSize = new System.Drawing.Size(500, 300);
+            this.Name = "MainWindow";
+            this.Text = "Auto shut down the computer © Artem Shevchenko";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button settingsButton;
+        private System.Windows.Forms.TextBox hours;
+        private System.Windows.Forms.TextBox minutes;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
